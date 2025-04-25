aefca3356cc87d50475c6011d475a0da4e622b57
krishnaverma111 <krishnaverma111786@gmail.com>
2025-04-25 21:01:29 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-// import Skills from './Skills'
+import Skills from './Skills'
 // import Project from './Project'
 // import Experience from './components/Experience'
 // import About from './components/About'
-import Home from './components/home'
+// import Home from './components/home'
 // import Tech from './components/Tech'
  
 
@@ -15,9 +15,9 @@ export default function App() {
     <div className='bg-blue-400'>
       <Navbar/>
      
-      <Home/> 
+      {/* <Home/>  */}
       {/* <Tech/> */}
-       {/* <Skills/> */}
+       <Skills/>
        {/* <Project/> */}
        {/* <Experience/> */}
        {/* <About/>   */}
